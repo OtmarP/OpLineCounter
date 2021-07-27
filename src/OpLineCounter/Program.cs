6f6e3fe679f2813e6fd22960d784b1df7d8925d0
Otmar Pilgerstorfer <Otmar.Pilgerstorfer@chello.at>
2021-07-27 09:27:08 +0200

    SummenZeile  # Files # Lines #1

--- src/OpLineCounter/Program.cs
+++ src/OpLineCounter/Program.cs
@@ -9,6 +9,7 @@ namespace OpLineCounter
     class Program
     {
         //--------------------------------------------
+        // Mo.26.07.2021 17:37:02 -op- SummenZeile  # Files # Lines
         // Do.22.07.2021 20:12:35 -op- mit SubDir /s
         // So.02.05.2021 16:39:26 -op- nach .\GitHub\OtmarP\OpLineCounter\src\OpLineCounter\
         // Mi.28.04.2021 09:35:40 -op- Cr (4.7.2)
@@ -22,6 +23,7 @@ namespace OpLineCounter
 #if DEBUG
             path = @"C:\Dev\TFS\Git\OpTools\MSPartnerCenterDownloader\MSPartnerCenter\bin\Debug\DATA";
             path = @"C:\Dev\Temp\MSPartnerCenter\DATA\20210426";
+            path = @"C:\Dev\Temp\MSPartnerCenter\DATA";
             //path = @".\";
             pattern = "*_*_*_*_*.*";
 #endif
@@ -48,8 +50,20 @@ namespace OpLineCounter
             var sd = System.IO.SearchOption.TopDirectoryOnly;
             if (subDir) { sd = System.IO.SearchOption.AllDirectories; }
 
+            int sumLines = 0;
+            int sumFiles = 0;
+
+            int len = 21;
+
             Console.WriteLine("Path: " + path + " Pattern: " + pattern + " SubDir: " + subDir);
             string trenn = "--------------------------------------------";
+            //              00000000011111111112222222222333333333344444444445
+            //              12345678901234567890123456789012345678901234567890
+            //              20210428_121314_D_30_ 123456789012
+            //              20210430_000700_W_12_          302
+            //              SQLUnitTest1.cs               2838
+            //              --------------------------------------------
+            //              Files: 1234                   1234 Lines
             Console.WriteLine(trenn);
             var dirList = System.IO.Directory.GetFiles(path, pattern, sd);
             for (int i = 0; i < dirList.Length; i++)
@@ -65,12 +79,11 @@ namespace OpLineCounter
                 }
                 catch (Exception ex)
                 {
-                    //throw;
+                    //
                 }
 
                 // 20210428_121314_D_30_ 123456789012
                 string disp = fileName;
-                var len = 21;
                 if (disp.Length > len)
                 {
                     disp = disp.Substring(0, len);
@@ -86,6 +99,7 @@ namespace OpLineCounter
 
                 //+ " ............................................................................";
                 int lines = countLines.Length - 1;  // - Header in .csv
+                sumLines += lines;
                 string dispLines = lines.ToString();
                 if (lines == 0)
                 {
@@ -93,10 +107,19 @@ namespace OpLineCounter
                 }
                 dispLines = dispLines.PadLeft(12);
                 Console.WriteLine(disp + " " + dispLines);
+                sumFiles++;
             }
 
             Console.WriteLine(trenn);
+
+            string disp2 = "Files: " + sumFiles.ToString() + "" + "".PadRight(len, ' ');
+            disp2 = disp2.Substring(0, len);
+            string dispLines2 = sumLines.ToString();
+            dispLines2 = dispLines2.PadLeft(12);
+            Console.WriteLine(disp2 +" "+ dispLines2 + " Lines");
+
 #if DEBUG
+            Console.WriteLine("Weiter mit jeder Taste...");
             Console.ReadKey();
 #endif
         }
